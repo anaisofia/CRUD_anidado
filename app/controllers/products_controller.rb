907472825ad3b390bc6a6ff27f433be8f00aed4a
Anaisofia <anaisrivas.26@gmail.com>
2019-06-17 20:50:16 -0400

    falta agregar bootstrap al show de categories, se incluyó el formulario para crear productos por categoría con nombre y precio, se incuye la lista de productos, y un botón para eliminar registros. También se incluyó el metodo destroy a las rutas y su controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to category_products_path
+    else
+      @category = @product.category
+      render "categories/show"
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to category_path(@product.category)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id)
+  end
 end
